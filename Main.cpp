84a1f01e847fdddcb061206f81a4ffbd4f37aac6
Yury <45543904+Overk1lls@users.noreply.github.com>
2019-01-08 12:34:34 +0100

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,153 +1,210 @@
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
+#include <ctime>
 
-#define l 100
+#define tSize 100
+#define width 40
+#define height 20
 
 using namespace std;
 
-bool GameOver = false;
-const int width = 40, height = 20;
-int x, y, i, j, fruitX, fruitY, score = 0, tailX[l], tailY[l], nTail = 0;
+class Snake {
+	public:
+		Snake() {
+			srand(time(NULL));
+			
+			nowTime = clock();
+			specFrTime = (float)nowTime / CLOCKS_PER_SEC + 60;
 
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-eDirection direct;
+			score = 0;
+			tailSize = 1;
+			isGameOver = false;
+			specAppears = false;
+			step = STOP;
 
-void Setup()
-{
-	GameOver = false;
-	direct = STOP;
-	x = width / 2 - 1;
-	y = height / 2 - 1;
-	fruitX = rand() % width;
-	fruitY = rand() % height;
-}
+			headX = width / 2 - 1;
+			headY = height / 2 - 1;
+			iFruitX = rand() % (width - 2) + 1;
+			iFruitY = rand() % (height - 2) + 1;
+			dFruitX = rand() % (width - 2) + 1;
+			dFruitY = rand() % (height - 2) + 1;
+			specFrX = rand() % (width - 2) + 1;
+			specFrY = rand() % (height - 2) + 1;
 
-void Draw()
-{
-	system("cls");
-	for (i = 0; i <= width; i++)
-		cout << "#";
-	cout << endl;
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			if (j == 0 || j == width - 1)
-				cout << "#";
-			if (i == y && j == x)
-				cout << "0";
-			else if (i == fruitY && j == fruitX)
-				cout << "F";
-			else
-			{
-				bool print = false;
-				for (int k = 0; k < nTail; k++)
-					if (tailX[k] == j && tailY[k] == i)
-					{
-						print = true;
-						cout << "o";
-					}
-				if (!print)
-					cout << " ";
+			while (iFruitX == dFruitX && iFruitY == dFruitY) {
+				dFruitX = rand() % (width - 2) + 1;
+				dFruitY = rand() % (height - 2) + 1;
+			}
+			while ((specFrX == iFruitX && specFrY == iFruitY) || (specFrX == dFruitX && specFrY == dFruitY)) {
+				specFrX = rand() % (width - 2) + 1;
+				specFrY = rand() % (height - 2) + 1;
 			}
 		}
-		cout << endl;
-	}
-	for (i = 0; i <= width; i++)
-		cout << "#";
-	cout << endl;
-	cout << "Score: " << score << endl;
-}
 
-void Input()
-{
-	if (_kbhit())
-		switch (_getch())
-		{
-		case 72:
-			direct = UP;
-			break;
-		case 75:
-			direct = LEFT;
-			break;
-		case 77:
-			direct = RIGHT;
-			break;
-		case 80:
-			direct = DOWN;
-			break;
-		case 27:
-			GameOver = true;
-			break;
+		bool GetGameOver() {
+			return this->isGameOver;
 		}
-}
 
-void Logic()
-{
-	int PrevX = tailX[0], PrevY = tailY[0], buffX, buffY;
+		void Redrawing() {
+			COORD cursPos;
+			cursPos.X = 0;
+			cursPos.Y = 0;
+			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursPos);
 
-	tailX[0] = x;
-	tailY[0] = y;
+			nowTime = clock();
+			if (((float)nowTime) / CLOCKS_PER_SEC > specFrTime)
+				specAppears = true;
 
-	for (i = 1; i < nTail; i++)
-	{
-		buffX = tailX[i];
-		buffY = tailY[i];
-		tailX[i] = PrevX;
-		tailY[i] = PrevY;
-		PrevX = buffX;
-		PrevY = buffY;
+			for (int i = 0; i <= width; i++)
+				cout << "#";
+			cout << endl;
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < width; j++) {
+					if (j == 0 || j == width - 1)
+						cout << "#";
+					if (i == headY && j == headX)
+						cout << "Z";
+					else if (i == iFruitY && j == iFruitX)
+						cout << "I";
+					else if (i == dFruitY && j == dFruitX)
+						cout << "D";
+					else if (specAppears && i == specFrY && j == specFrX)
+						cout << "S";
+					else {
+						bool isSpace = true;
+						for (int k = 0; k < tailSize; k++)
+							if (tailX[k] == j && tailY[k] == i) {
+								isSpace = false;
+								cout << "z";
+							}
+						if (isSpace)
+							cout << " ";
+					}
+				}
+				cout << endl;
+			}
+			for (int i = 0; i <= width; i++)
+				cout << "#";
+			cout << endl;
+			cout << "Score: " << score << endl;
+		}
+
+		void Move() {
+			if (_kbhit())
+				switch (_getch()) {
+					case 72:
+						step = UP;
+						break;
+					case 75:
+						step = LEFT;
+						break;
+					case 77:
+						step = RIGHT;
+						break;
+					case 80:
+						step = DOWN;
+						break;
+					case 27:
+						isGameOver = true;
+						break;
+				}
+		}
+
+		void Loot() {
+			switch (step) {
+				case UP:
+					headY--;
+					break;
+				case LEFT:
+					headX--;
+					break;
+				case RIGHT:
+					headX++;
+					break;
+				case DOWN:
+					headY++;
+					break;
+			}
+			int PrevX = tailX[0], PrevY = tailY[0], buffX, buffY;
+
+			tailX[0] = headX;
+			tailY[0] = headY;
+
+			for (int i = 1; i < tailSize; i++) {
+				buffX = tailX[i];
+				buffY = tailY[i];
+				tailX[i] = PrevX;
+				tailY[i] = PrevY;
+				PrevX = buffX;
+				PrevY = buffY;
+			}
+			for (int i = 1; i < tailSize; i++)
+				if (tailX[i] == headX && tailY[i] == headY)
+					isGameOver = true;
+
+			if (headX >= width - 1)
+				headX = 0;
+			else if (headX < 0)
+				headX = width - 1;
+			if (headY > height)
+				headY = 0;
+			else if (headY < 0)
+				headY = height - 1;
+
+			if (headX == iFruitX && headY == iFruitY) {
+				score++;
+				tailSize++;
+				iFruitX = rand() % (width - 2) + 1;
+				iFruitY = rand() % (height - 2) + 1;
+				while (iFruitX == dFruitX)
+					iFruitX = rand() % (width - 2) + 1;
+				while (iFruitY == dFruitY)
+					iFruitY = rand() % (height - 2) + 1;
+			}
+			else if (headX == dFruitX && headY == dFruitY) {
+				if (score == 0)
+					isGameOver = true;
+				else {
+					score--;
+					tailSize--;
+					dFruitX = rand() % (width - 2) + 1;
+					dFruitY = rand() % (height - 2) + 1;
+					while (iFruitX == dFruitX)
+						dFruitX = rand() % (width - 2) + 1;
+					while (iFruitY == dFruitY)
+						dFruitY = rand() % (height - 2) + 1;
+				}
+			}
+			else if (headX == specFrX && headY == specFrY && specAppears) {
+				specFrTime = ((float)nowTime) / CLOCKS_PER_SEC + 60;
+				score += 3;
+				tailSize += 3;
+				specAppears = false;
+				specFrX = rand() % (width - 2) + 1;
+				specFrY = rand() % (height - 2) + 1;
+				while ((specFrX == iFruitX && specFrY == iFruitY) || (specFrX == dFruitX && specFrY == dFruitY)) {
+					specFrX = rand() % (width - 2) + 1;
+					specFrY = rand() % (height - 2) + 1;
+				}
+			}
+		}
+
+	private:
+		enum eStep { STOP = 0, LEFT, RIGHT, UP, DOWN };
+		eStep step;
+		bool isGameOver, specAppears;
+		int headX, headY, iFruitX, iFruitY, dFruitX, dFruitY, specFrX, specFrY, tailX[tSize], tailY[tSize], score, tailSize;
+		clock_t nowTime;
+		int specFrTime;
+};
+
+int main() {
+	Snake game;
+	while (!game.GetGameOver()) {
+		game.Redrawing();
+		game.Move();
+		game.Loot();
 	}
-
-	for (i = 1; i < nTail; i++)
-		if (tailX[i] == x && tailY[i] == y)
-			GameOver = true;
-
-	switch (direct)
-	{
-	case UP:
-		y--;
-		break;
-	case LEFT:
-		x--;
-		break;
-	case RIGHT:
-		x++;
-		break;
-	case DOWN:
-		y++;
-		break;
-	}
-
-	if (x >= width - 1)
-		x = 0;
-	else if (x < 0)
-		x = width - 1;
-	if (y > height)
-		y = 0;
-	else if (y < 0)
-		y = height - 1;
-
-	if (x == fruitX && y == fruitY)
-	{
-		score++;
-		nTail++;
-		fruitX = rand() % width;
-		fruitY = rand() % height;
-	}
-}
-
-int main()
-{
-	Setup();
-
-	while (!GameOver)
-	{
-		Draw();
-		Input();
-		Logic();
-	}
-
 	return 0;
 }
